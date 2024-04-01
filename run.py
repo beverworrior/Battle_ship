0d0fc01913254f9f644506ebed0f56724abef12a
Jan-Robert Nielsen <robertnielsen33@gmail.com>
2024-04-01 09:25:14 +0000

    Made a class to creat the board and ships

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class BattleshipGame:
+    def __init__(self, size=5, num_ships=3):
+        self.size = size
+        self.num_ships = num_ships
+        self.board = [['O' for _ in range(size)] for _ in range(size)]
+        self.ships = []
+        self.generate_ships()
+    print("BattleshipGame")
+    
